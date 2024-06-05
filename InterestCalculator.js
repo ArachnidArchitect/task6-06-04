b15854f3f2651c4fe14b586ae93325d2620e346a
ArachnidArchitect <proffesionalworkingstuff@gmail.com>
2024-06-05 10:26:31 +0200

    html is added to task 6

--- InterestCalculator.js
+++ InterestCalculator.js
@@ -1,3 +1,12 @@
+let display = document.querySelector('#display')
+let input1 = document.querySelector('#amount')
+let input2 = document.querySelector('#interestVal')
+let button = document.querySelector('button')
+
+
+button.addEventListener('click', ()=>{
+    interest(input1.value, input2.value)
+})
 function interest(val1, val2) {
     if (val2.includes('%')) {
         // console.log('checking:'+val2.legnth)
@@ -5,12 +14,14 @@ function interest(val1, val2) {
         console.log(interestValue)
         answer =  (val1 * interestValue)/100
    console.log(answer)
+   display.innerText ='Interest value: ' +answer
     }else{
-         let  = val2
+         let interestValue = val2
          answer =  (val1 * interestValue)/100
    console.log(answer)
+   display.innerText = 'Interest value: ' +answer
     }
     
 }
-interest(200,"2%")
+
 
